e96cf6f79823ebb4164b04d2ee78690f7d0d995e
Madhavi Solanki <madhavisolanki02@gmail.com>
2026-02-07 00:09:24 +0530

    Add files via upload

---
+++ Move_all_Zeros_to_the_end_of_the_array.java
@@ -0,0 +1,26 @@
+package Practice_26;
+
+import java.util.Scanner;
+
+public class Move_all_Zeros_to_the_end_of_the_array {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
